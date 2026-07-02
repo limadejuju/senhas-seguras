15b1a53597317c8c9a5b1fc091fa1b582a61738f
limadejuju <lima.juliano30@escola.pr.gov.br>
2026-07-02 12:17:09 -0300

    11

--- main.js
+++ main.js
@@ -63,7 +63,9 @@ function classificaSenha(){
     forcaSenha.classList.remove('fraca','media','forte');
     if (tamanhoSenha > 11){
         forcaSenha.classList.add('forte');
-    } else {
+    } else if (tamanhoSenha > 5 && tamanhoSenha < 12 ) {
         forcaSenha.classList.add('media');
-    } 
+    } else if (tamanhoSenha <= 5){
+        forcaSenha.classList.add('fraca');
+    }
 }
