978bcd82df7789d9bca7f4d5477c39ba9cb94729
limadejuju <lima.juliano30@escola.pr.gov.br>
2026-07-02 08:49:17 -0300

    11

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
 geraSenha();
 
 function geraSenha(){
-    let senha = '' ”;
+    let senha = '' ;
     for (let i = 0; i < tamanhoSenha;i++){
         let numeroAleatorio = Math.random()*letrasMaiusculas.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
